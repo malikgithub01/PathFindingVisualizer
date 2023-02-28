78fe626fd96854ef387a036fcdbdc8d2e128b04e
malikgithub01 <112425419+malikgithub01@users.noreply.github.com>
2023-03-01 00:31:51 +0100

    Refactoring and Cannot click nodes when Visualizing

--- src/visualizer/PathVisualizer.js
+++ src/visualizer/PathVisualizer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import Node from '../components/Node';
+import Node from '../components/node/Node';
 import { dijkstra, getNodesInShortestPathOrder } from '../algorithms/dijkstra';
+import {getInitialGrid, getNewGridWithWallToggled} from '../components/grid/grid'
 import './PathVisualizer.css'
 
 
@@ -9,49 +10,11 @@ const START_NODE_COL = 15;
 const FINISH_NODE_ROW = 10;
 const FINISH_NODE_COL = 35;
 
-const getInitialGrid = () => {
-    const grid = [];
-    for (let row = 0; row < 20; row++) {
-        const currentRow = [];
-        for (let col = 0; col < 50; col++) {
-            currentRow.push(createNode(col, row));
-        }
-        grid.push(currentRow);
-        console.log(grid)
-    }
-    return grid;
-};
-
-const createNode = (col, row) => {
-    return {
-        col,
-        row,
-        isStart: row === START_NODE_ROW && col === START_NODE_COL,
-        isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
-        distance: Infinity,
-        isVisited: false,
-        isWall: false,
-        previousNode: null,
-    };
-};
-
-const getNewGridWithWallToggled = (grid, row, col) => {
-    const newGrid = grid.slice();
-    const node = newGrid[row][col];
-    const newNode = {
-        ...node,
-        isWall: !node.isWall,
-    };
-    newGrid[row][col] = newNode;
-    return newGrid;
-};
-
-
-
 const PathVisualizer = () => {
 
     const [grid, setGrid] = useState([]);
     const [mouseIsPressed, setMouseIsPressed] = useState(false);
+    const [mouseDisabled, setMouseDisabled] = useState(false)
 
     useEffect(() => {
         const initialGrid = getInitialGrid();
@@ -115,6 +78,7 @@ const PathVisualizer = () => {
     };
 
     const visualizeDijkstra = () => {
+        setMouseDisabled(true)
         const startNode = grid[START_NODE_ROW][START_NODE_COL];
         const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
         const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
@@ -123,7 +87,7 @@ const PathVisualizer = () => {
     };
 
     const clearGrid = () => {
-        console.log("grid",typeof grid ,grid)
+        setMouseDisabled(false)
         const initialGrid = getInitialGrid();
         setGrid(initialGrid);
 
@@ -165,9 +129,9 @@ const PathVisualizer = () => {
                                         isStart={isStart}
                                         isWall={isWall}
                                         mouseIsPressed={mouseIsPressed}
-                                        onMouseDown={(row, col) => handleMouseDown(row, col)}
-                                        onMouseEnter={(row, col) => handleMouseEnter(row, col)}
-                                        onMouseUp={() => handleMouseUp()}
+                                        onMouseDown={mouseDisabled ? null : (row, col) => handleMouseDown(row, col)}
+                                        onMouseEnter={mouseDisabled ? null : (row, col) => handleMouseEnter(row, col)}
+                                        onMouseUp={mouseDisabled ? null : () => handleMouseUp()}
                                         row={row}
                                     />
                                 );
